8c6666e4b71b86d3f14313812768d8f9e4e66ef9
zhangquansongjava@163.com <zhangquansongjava@163.com>
2020-03-05 15:11:47 +0800

    springcloudgateway 初始化提交

---
+++ cloud-dy-user/src/main/java/com/cloud/dy/user/CloudDyUserApplication.java
@@ -0,0 +1,48 @@
+package com.cloud.dy.user;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+@RefreshScope
+@EnableFeignClients
+public class CloudDyUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDyUserApplication.class, args);
+    }
+
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Value("${version}")
+    private String version;
+    @Value("${cloudDream}")
+    private String cloudDream;
+    @Value("${capacity}")
+    private int capacity;
+    @Value("${refillTokens}")
+    private int refillTokens;
+    @Value("${refillDuration}")
+    private int refillDuration;
+
+    @RequestMapping("/getVersion")
+    public String getVersion() {
+        return version;
+    }
+}
