26346827fc9a623206614f2e7aee043fde6b021c
zhangquansongjava@163.com <zhangquansongjava@163.com>
2020-03-12 19:00:55 +0800

    网关提交

--- cloud-dy-gateway/src/main/java/com/cloud/dy/gateway/CloudDyGatewayApplication.java
+++ cloud-dy-gateway/src/main/java/com/cloud/dy/gateway/CloudDyGatewayApplication.java
@@ -1,5 +1,12 @@
 package com.cloud.dy.gateway;
 
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +15,13 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 @RestController
@@ -44,5 +55,23 @@ public class CloudDyGatewayApplication {
     @RequestMapping("/getVersion")
     public String getVersion() {
         return version;
+    }
+
+    @GetMapping("/api")
+    @SentinelResource("api")
+    public Set<ApiDefinition> apiRules() {
+        return GatewayApiDefinitionManager.getApiDefinitions();
+    }
+
+    @GetMapping("/gateway")
+    @SentinelResource("gateway")
+    public Set<GatewayFlowRule> apiGateway() {
+        return GatewayRuleManager.getRules();
+    }
+
+    @GetMapping("/flow")
+    @SentinelResource("flow")
+    public List<FlowRule> apiFlow() {
+        return FlowRuleManager.getRules();
     }
 }
---
+++ cloud-dy-gateway/src/main/java/com/cloud/dy/gateway/filter/GatewayConfiguration.java
@@ -0,0 +1,35 @@
+package com.cloud.dy.gateway.filter;
+
+
+/**
+ * @ClassName SentinelGatewayFilter
+ * @Description TODO
+ * @Auther zhangquansong
+ * @Date 2020/3/11 21:38
+ * @Version 1.0
+ **/
+//@Configuration
+public class GatewayConfiguration {
+
+    /*private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GatewayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        // Register the block exception handler for Spring Cloud Gateway.
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    @Bean
+    @Order(-1)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }*/
+}
