c6b63b732d4a2f231550564b8743b4b710e749f6
zhangquansongjava@163.com <zhangquansongjava@163.com>
2020-05-19 18:53:33 +0800

    vue el admin

---
+++ cloud-dy-web/src/views/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="dashboard-container">
+    <component :is="currentRole" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import dy from './dy'
+import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: { dy, editorDashboard },
+  data() {
+    return {
+      // currentRole: 'adminDashboard'
+      currentRole: 'dy'
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
+</script>
