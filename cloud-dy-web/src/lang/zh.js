d6f5bf2a1e84e3b63875dde7ad229970a31c7924
zhangquansongjava@163.com <zhangquansongjava@163.com>
2020-05-19 19:33:27 +0800

    vue el admin

--- cloud-dy-web/src/lang/zh.js
+++ cloud-dy-web/src/lang/zh.js
@@ -61,7 +61,15 @@ export default {
     theme: '换肤',
     clipboardDemo: 'Clipboard',
     i18n: '国际化',
-    externalLink: '外链'
+    externalLink: '外链',
+    user: '人员管理',
+        menu1: '菜单1',
+        'menu1-1': '菜单 1-1',
+        'menu1-2': '菜单 1-2',
+        'menu1-2-1': '菜单 1-2-1',
+        'menu1-2-2': '菜单 1-2-2',
+        'menu1-3': '菜单 1-3',
+        menu2: '菜单 2',
   },
   navbar: {
     logOut: '退出登录',
